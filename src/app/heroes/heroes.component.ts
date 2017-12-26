29d051fec2802f0ed6179f123c187906da5a222c
Rija RAMAMPIANDRA <rijaramampiandra@gmail.com>
2017-12-26 15:39:43 +0300

    <dev> https://angular.io/tutorial/toh-pt4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
+	selector : 'app-heroes',
+	templateUrl : './heroes.component.html',
+	styleUrls : [ './heroes.component.css' ]
+	})
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+		heroes: Hero[];
 
-  selectedHero: Hero;
+		selectedHero: Hero;
+
+		
+		constructor(private heroService: HeroService) { }
+
+		ngOnInit() {
+			this.getHeroes();
+		}
+
+		onSelect(hero: Hero): void {
+			this.selectedHero = hero;
+		}
+		
+		getHeroes(): void {
+			  this.heroes = this.heroService.getHeroes();
+			}
+	}
 
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-}
-
-
-/*
-Copyright 2017 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+	/*
+	Copyright 2017 Google Inc. All Rights Reserved.
+	Use of this source code is governed by an MIT-style license that
+	can be found in the LICENSE file at http://angular.io/license
+	*/
